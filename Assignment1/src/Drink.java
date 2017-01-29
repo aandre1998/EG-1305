68f41032dffbac5485d6e3135bad8f216ea60014
aktunc-STMU <aktunc@gmail.com>
2017-01-28 18:07:29 -0600

    Incorrect result

--- Assignment1/src/Drink.java
+++ Assignment1/src/Drink.java
@@ -7,5 +7,6 @@ public class Drink
 		System.out.println("Number of customers: " + customers);
 		System.out.println("Number of customers who purchased one or more energy drink per week: " + (customers * .14));
 		System.out.println("Number of customers who prefer citrus flavored energy drinks: " + (customers * .64));
+		//Last one is incorrect. It should be customers * .64 * .14
 	}
 }
